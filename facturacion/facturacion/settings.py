1bc1dc15e866ebe086b4c06c5b906f082e4d4261
fedexin40 <federico.cruz.lozano@hotmail.com>
2024-10-20 18:52:49 -0600

    Fix django issue

--- facturacion/facturacion/settings.py
+++ facturacion/facturacion/settings.py
@@ -130,3 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://facturacionadmin.proyecto705.com']
